1f6d5816a3fd484f99d61e19067a8241adee2942
towhidulislamalif <towhidulislamalif@gmail.com>
2022-12-04 23:05:08 +0600

    AppBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <div className="app"></div>;
-}
+import React from 'react';
+import {
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Stack,
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+} from '@mui/material';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography>Album layout</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default App;
